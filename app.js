0afb999df7c9c86660a56697c6f08f698082feee
Andy Chen <ajiajiechen@gmail.com>
2020-04-02 18:07:00 -0700

    added app.js to use express for get and post

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+var path = require("path");
+app.use(express.static('public'))
+
+const PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
